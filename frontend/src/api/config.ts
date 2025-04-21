4bdc7acd7ecd4a8063d705397b78ba8bab891fe4
juanglc <juanguiloco3@gmail.com>
2025-04-21 12:04:49 -0500

    Commit

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,4 +1,4 @@
 // src/api/config.ts
-const API_URL = import.meta.env.VITE_API_URL || "https://blog-hlkv.onrender.com/";
+const API_URL = import.meta.env.VITE_API_URL || "https://blog-hlkv.onrender.com";
 // Remove trailing slash if present
 export default API_URL;
