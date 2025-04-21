b491efaf67f89c6bd1a66c494114638b6af93f60
juanglc <juanguiloco3@gmail.com>
2025-04-21 10:15:51 -0500

    Commit

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -6,9 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+path = '/home/juanglc/blog_project/backend/'
 import os
+import sys
 
+if path not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
